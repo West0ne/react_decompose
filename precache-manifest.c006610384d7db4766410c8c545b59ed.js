f56af9aa9139030fedcbfe3453cc84146c1b68ee
westone <west0ne@MacBook-Pro-Oleg.local>
2021-08-29 11:25:18 +0300

    Updates

---
+++ precache-manifest.c006610384d7db4766410c8c545b59ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0717903101afec15acbf0f231eea76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ae72c28a6c4172a69cdd",
+    "url": "./static/css/main.924abda3.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae72c28a6c4172a69cdd",
+    "url": "./static/js/main.5534bf38.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
